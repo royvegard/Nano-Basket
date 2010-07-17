bee7fa33d7d59fc00394fcb6b87519699c240cef
Roy Vegard Ovesen <roy.v.ovesen@haugnett.no>
2010-07-18 01:24:59 +0200

    Added license and copyright notices, and .gitignore.
    Signed-off-by: Roy Vegard Ovesen <roy.v.ovesen@haugnett.no>

--- nano_basket_backend.py
+++ nano_basket_backend.py
@@ -1,5 +1,24 @@
 # -*- coding: utf-8 -*-
 
+#   Nano Basket
+#   Copyright (C) 2010 Roy Vegard Ovesen <roy.v.ovesen@haugnett.no>
+
+
+#   This file is part of Nano Basket.
+#
+#   Nano Basket is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Nano Basket is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with Nano Basket.  If not, see <http://www.gnu.org/licenses/>.
+
 class Nano_Kontrol_Common:
    """Common parameters from TABLE 1 of 'nanoKONTROL MIDI Implementation'
    file available from Korg."""
